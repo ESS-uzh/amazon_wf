704a6b98b4d09fa1a2fba85857222ae68199aa53
sparkvilla <diego.villamaina@gmail.com>
2021-06-15 06:36:04 +0000

    use pool of connections to optimize interaction with the db

--- amazon_wf/database.py
+++ amazon_wf/database.py
@@ -1,10 +1,46 @@
-import psycopg2
-import json
+from psycopg2 import pool
 import pdb
 
-with open('../../db_amazon_credentials.json', "r") as read_file:
-    db = json.load(read_file)
 
-def connect():
-    return psycopg2.connect(user=db['user'], password=db['pwd'],
-            database=db['database'], host=db['host'])
+
+class Database:
+    connection_pool = None
+
+    @staticmethod
+    def initialise(**kwargs):
+        Database.connection_pool = pool.SimpleConnectionPool(1,
+                                                            10,
+                                                            **kwargs)
+    @staticmethod
+    def get_connection():
+        return Database.connection_pool.getconn()
+
+
+    @staticmethod
+    def return_connection(connection):
+        Database.connection_pool.putconn(connection)
+
+
+    @staticmethod
+    def close_all_connections():
+        Database.connection_pool.closeall()
+
+
+class CursorFromConnectionPool:
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.conn = Database.get_connection()
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        if exception_value:
+            self.conn.rollback()
+        else:
+            self.cursor.close()
+            self.conn.commit()
+        Database.return_connection(self.conn)
+
